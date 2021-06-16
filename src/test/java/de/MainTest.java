1dded527b38b409d9c4f7daabc747128ad4d9341
Ayrat <ayrat1@mail.ru>
2021-06-16 23:36:17 +0300

    add selenium grid

--- src/test/java/de/MainTest.java
+++ src/test/java/de/MainTest.java
@@ -5,21 +5,39 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.util.List;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class MainTest {
-    static WebDriver driver;
+    private static WebDriver driver;
 
     @BeforeClass
-    public static void setup() {
+    public static void setup() throws MalformedURLException {
 //        System.setProperty("webdriver.chrome.driver", "/Users/Vaio/Downloads/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-        driver = new ChromeDriver();
+
+        // 1. Define desired capabilities
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setBrowserName("chrome");
+        cap.setPlatform(Platform.WINDOWS);
+        // 2. Chrome options definition
+        ChromeOptions options = new ChromeOptions();
+        options.merge(cap);
+        // 3. Set driver & hubUrl
+        String hubUrl = "http://192.168.1.67:4444/wd/hub";
+        driver = new RemoteWebDriver(new URL(hubUrl), options);
+
+//        System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+//        driver = new ChromeDriver();
+        // 4. Set request URL
         driver.get("https://passport.yandex.ru");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -28,8 +46,9 @@ public class MainTest {
     @Test
     public void mainTest() {
         String title = driver.getTitle();
-        System.out.println(title);
+        System.out.println(driver.getTitle());
         Assert.assertTrue(title.equals("Авторизация"));
+
         driver.findElement(By.xpath("//*[@id=\'passp-field-login\']")).sendKeys("AIRIZZIO");
         driver.findElement(By.xpath("//*[@id=\'root\']/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[3]/button")).click();
         driver.findElement(By.xpath("//*[@id=\'passp-field-passwd\']")).sendKeys("Diana2013");
@@ -39,14 +58,17 @@ public class MainTest {
         driver.findElement(By.xpath("//*[@id=\'root\']/div/div[2]/div[1]/div/div/div/div/ul/ul/li[1]/a/span")).click();
 
         int size = driver.findElements(By.partialLinkText("Simbirsoft Тестовое задание")).size();
-        System.out.println(size);
+        Assert.assertEquals(2, size);
 
         driver.findElement(By.xpath("//*[@id=\'nb-1\']/body/div[2]/div[8]/div/div[3]/div[2]/div[2]/div/div/a/span")).click();
         driver.findElement(By.xpath("//*[@id=\'nb-1\']/body/div[2]/div[11]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[1]/div[1]/div/div/div/div/div")).sendKeys("airizzio@yandex.ru");
         driver.findElement(By.xpath("//*[@id=\'nb-1\']/body/div[2]/div[11]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/div[1]/div[3]/div/div/input")).sendKeys("Simbirsoft Тестовое задание. Хайруллин");
         driver.findElement(By.xpath("//*[@id=\'cke_1_contents\']/div")).sendKeys(String.valueOf(size));
-        driver.findElement(By.xpath("//*[@id=\'nb-1\']/body/div[2]/div[11]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/div[2]/div/div[1]/div[1]/button")).click();
 
+        WebElement element = driver.findElement(By.xpath("//*[@id=\'nb-1\']/body/div[2]/div[11]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/div[2]/div/div[1]/div[1]/button"));
+        Assert.assertNotNull(element);
+
+        element.click();
     }
 
     @AfterClass
@@ -55,3 +77,7 @@ public class MainTest {
     }
 
 }
+
+//java -jar selenium-server-standalone-3.141.59.jar -role hub
+
+//java -Dwebdriver.chrome.driver="C:/Users/Vaio/Downloads/chromedriver.exe" -jar selenium-server-standalone-3.141.59.jar -role node -hub http://192.168.1.67:4444/grid/register
